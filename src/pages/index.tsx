7f0a56f918d405c7f4c10acdfb1aab050ba0f037
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-23 13:58:35 -0300

    Migrate to Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+
+function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default Home;
