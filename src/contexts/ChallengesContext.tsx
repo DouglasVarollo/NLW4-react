c7b45456e4c82ea65c01d53d99d6147e45f925a0
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-25 12:38:42 -0300

    Plays a sound when a cycle ends

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -72,6 +72,8 @@ export function ChallengesProvider(props: ChallengesProviderProps) {
 
     setActiveChallenge(challenge);
 
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio', {
         body: `Valendo ${challenge.amount}xp!`
